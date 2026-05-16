06c9ce8294e9ccf50e5d0791268772510ee42215
Jim Randell <jim.randell@gmail.com>
2026-05-16 11:20:57 +0100

    added some more efficient algorithms: (simplified) LMM, Cornachia (and sqrtmod via Tonelli-Shanks).

--- pells.py
+++ pells.py
@@ -10,12 +10,12 @@
 from __future__ import print_function
 
 from enigma import (
-  enigma, irange, inf, is_square, sqrtf, sqrtc, gcd, div, divf, divc, multiply,
-  prime_factor, divisors_pairs, sq, rev, merge, as_int, cache, printf,
+  enigma, irange, inf, is_square, sqrtf, sqrtc, gcd, div, divf, divc, multiply, invmod,
+  divisors_pairs, sq, rev, merge, multiset, cproduct, crt, as_int, cache, printf,
 )
 
 __author__ = "Jim Randell <jim.randell@gmail.com>"
-__version__ = "2025-12-16"
+__version__ = "2026-05-14"
 
 pells = enigma.module(__name__)
 verbose = ('v' in enigma._PY_ENIGMA)
@@ -116,6 +116,9 @@ def pells_sol(D, xy, uv):
     yield (x, y)
     (x, y) = (A * x + B * y, C * x + A * y)
 
+# threshold above which we switch to LMM (or issue a warning)
+pells_threshold = 100000
+
 # find all (X, Y) solutions for: X^2 - D.Y^2 = N [D > 0, non-square; N != 0]
 def pellsN(D, N):
   # find the fundamental solution to the resolvant: X^2 - D.Y^2 = 1
@@ -127,21 +130,241 @@ def pellsN(D, N):
     (a, b) = (0, sqrtf(divc(N * (u - 1), 2 * D)))
   else:
     (a, b) = (sqrtc(divc(-N, D)), sqrtf(divc(-N * (u + 1), 2 * D)))
-  if verbose or b - a > 100000: printf("pells: attempting brute force y = [{a} .. {b}]")
+
+  fn = None
+  if b - a > pells_threshold:
+    rD = sqrtf(D)
+    if N * N < D and rD * rD < D:
+      if verbose: printf("[pells] switching to simplified LMM (rather then brute force y = [{a} .. {b}])")
+      fn = pells_LMMs(D, N, rD)
+  if not fn:
+    if verbose or b - a > pells_threshold: printf("[pells] attempting brute force y = [{a} .. {b}]")
+    fn = pells_BF(D, N, a, b, u, v)
 
   # find solution families
-  sols = list()
+  sols = (pells_sol(D, (x, y), (u, v)) for (x, y) in fn)
+  return merge(sols, uniq=1)
+
+# brute force
+def pells_BF(D, N, a, b, u, v):
   for y in irange(a, b):
     x = is_square(N + D * y * y)
     if x is not None:
-      sols.append(pells_sol(D, (x, y), (u, v)))
+      yield (x, y)
       # find minimal positive equivalent solution for (-x, y)
       (X, Y) = (x * u - y * v * D, x * v - y * u)
       if X < 0: (X, Y) = (-X, -Y)
       if (X, Y) != (x, y):
-        sols.append(pells_sol(D, (X, Y), (u, v)))
+        yield (X, Y)
 
-  return merge(sols, uniq=1)
+# simplified LMM (for N < sqrt(D) and D is not square)
+def pells_LMMs(D, N, rD):
+  fs = dict()
+  for (f, _) in divisors_pairs(N):
+    (m, r) = divmod(N, f * f)
+    if r == 0:
+      fs[m] = f
+  (P, Q, G0, G1, B0, B1) = (0, 1, 0, 1, 1, 0)
+  while 1:
+    for _ in (0, 1):
+      a = (P + rD) // Q
+      P = a * Q - P
+      Q = (D - P * P) // Q
+      (G0, G1, B0, B1) = (G1, a * G1 + G0, B1, a * B1 + B0)
+      f = fs.get(G1 * G1 - D * B1 * B1)
+      if f is not None:
+        yield (f * G1, f * B1)
+    if Q == 1: break
+
+######################################################################
+
+# Tonelli-Shanks algorithm for modular square roots
+
+# we could use:
+# sqrtmod = lambda a, m, fs=None: enigma.poly_roots_mod.sqrtmod(a, m)
+
+# but the following is more efficient for large numbers (and is based on sympy.ntheory.sqrt_mod_iter)
+
+# we need an efficient factorisation implementation
+prime_factor = enigma.prime_factor
+# although for large numbers we might use:
+# prime_factor = enigma.partial(enigma.prime_factor_h, ps=enigma.primes, end=1000000, mr=1)
+
+# check for numbers that have modular square roots (i.e. is a quadratic residue)
+# there are several functions that can do this: legendre(), jacobi(), kronecker()
+
+# we use the Legendre symbol (a|p); a > 0; p is an odd prime
+# = 0 if a = 0 (mod p)
+# = 1 if a is a quadratic residue (mod p)
+# = -1 otherwise
+def legendre(a, p):
+  r = pow(a, (p - 1) // 2, p)  # calculate using Euler's criteria
+  return (-1 if r == p - 1 else r)
+
+# check if a number is a quadratic residue
+qr_check = legendre
+
+# Tonelli-Shanks algorithm for modular square roots
+# returns x, such that x^2 = a (mod p)
+# -x (mod p) is also a root
+# p must be prime, with p % 8 = 1
+def tonelli_shanks(a, p):
+  assert p % 8 == 1
+
+  # s = gmpy2.bitscan1(p - 1)
+  # t = p >> s
+  (s, t) = (0, p - 1)
+  while t & 1 == 0:
+    t >>= 1
+    s += 1
+
+  # find a non-quadratic residue
+  if p % 12 == 5:
+    d = 3
+  elif p % 5 in {2, 3}:
+    d = 5
+  else:
+    d = 6
+    while d < p:
+      if qr_check(d, p) == -1: break
+      d += 1
+
+  (A, D, m) = (pow(a, t, p), pow(d, t, p), 0)
+  for i in range(s):
+    u = (A * pow(D, m, p)) % p
+    u = pow(u, 1 << (s - 1 - i), p)
+    if u % p == p - 1:
+      m += (1 << i)
+  return (pow(a, (t + 1) // 2, p) * pow(D, m // 2, p)) % p
+
+# now find all solutions to x^2 = a (mod p^k)
+
+# case 1: a and p are coprime (there are at most 4 roots)
+# returns a list of the quadratic residues
+def _sqrtmodp1(a, p, k=1):
+  assert a % p != 0
+
+  pk = p**k
+  a %= pk
+  if p == 2:
+    if a % 8 != 1: return
+    if k < 4: return list(range(1, pk, 2))
+    # hensel lift
+    r = 1
+    for n in range(3, k):
+      if ((r*r - a) >> n) & 1:
+        r += 1 << (n - 1)
+    h = r + (1 << (k - 1))
+    return [r, pk - r, h % pk, -h % pk]
+
+  if qr_check(a, p) != 1: return []
+  if p % 4 == 3:
+    r = pow(a, (p + 1) // 4, p)
+  elif p % 8 == 5:
+    r = pow(a, (p + 3) // 8, p)
+    if pow(r, 2, p) != a % p:
+      r = (r * pow(2, (p - 1) // 4, p)) % p
+  else:
+    r = tonelli_shanks(a, p)
+
+  if k > 1:
+    # hensel lift
+    px = p
+    for _ in range(k.bit_length() - 1):
+      px *= px
+      r = (r - (r*r - a) * invmod(2*r, px)) % px
+    if k & (k - 1):
+      # k is not a power of 2
+      r = (r - (r*r - a) * invmod(2*r, px)) % pk
+
+  return [r, pk - r]
+
+# case 2: a % p = 0, there may be many solutions
+# returns an iterable or None
+def _sqrtmodp2(a, p, k):
+  assert a % p == 0
+  pk = p**k
+  a %= pk
+
+  if a == 0: return range(0, pk, p**((n + 1) // 2))
+
+  m = 0
+  while a % p == 0:
+    (a, m) = (a // p, m + 1)
+  if m % 2 == 1: return None
+  rs = _sqrtmodp1(a, p, k - m)
+  if not rs: return None
+  m //= 2
+  return (x for r in rs for x in range(r * p**m, pk, p**(k - m)))
+
+# combine the cases
+def sqrtmodp(a, p, k):
+  return (_sqrtmodp1(a, p, k) if a % p != 0 else _sqrtmodp2(a, p, k))
+
+# find square roots of <a> mod <m> (i.e x such that pow(x, 2, m) = a (mod m)
+# fs is (optionally) the prime factorisation of n
+def sqrtmod(a, m, fs=None):
+  # solve for each prime power in the factorisation
+  if not fs: fs = multiset.from_pairs(prime_factor(m))
+  (roots, ms) = (list(), list())
+  for (p, k) in fs.items():
+    rs = sqrtmodp(a, p, k)
+    if not rs: return []
+    roots.append(rs)
+    ms.append(p**k)
+  if roots:
+    # combine the results using CRT
+    if len(roots) == 1: return roots[0]
+    return (crt(vs, ms).x for vs in cproduct(roots))
+
+######################################################################
+
+# Cornacchia's algorithm:
+# find all (X, Y) solutions for: X^2 + D.Y^2 = N [0 < D < N, gcd(D, N) = 1]
+
+# generate primitive (= co-prime) solutions to: X^2 + D.Y^2 = N
+# fs is (optionally) the prime factorisation of N
+def cornacchia_primitive(D, N, fs=None):
+  # deal with some simple cases
+  if N == 1:
+    yield (1, 0)
+    if D == 1: yield (0, 1)
+    return
+  if D == N: yield (0, 1)
+  r = is_square(N)
+  if r is not None: yield (r, 0)
+  if D >= N: return
+
+  # now the general case ...
+  assert 0 < D < N and gcd(D, N) == 1
+  # look for square roots of -D (mod N)
+  for z in sqrtmod(-D, N, fs):
+    # we don't need roots > N/2
+    if 2 * z > N: continue
+    # euclidean descent
+    (r, X) = (N, z)
+    while X * X >= N:
+      (r, X) = (X, r % X)
+    # validate candidate solution
+    Y = is_square(div(N - X * X, D))
+    if Y is not None:
+      yield (X, Y)
+      if D == 1: yield (Y, X)
+
+# find all solutions for X^2 + D.Y^2 = N
+def cornacchia(D, N):
+  # collect primitive solutions
+  ss = set(cornacchia_primitive(D, N))
+  # find prime factors of N
+  fs = multiset.from_pairs(prime_factor(N))
+  # determine non-primitive solutions
+  sqs = multiset.from_pairs((p, e // 2) for (p, e) in fs.items())
+  for vs in sqs.subsets(min_size=1):
+    g = multiply((v if k == 1 else v**k) for (v, k) in vs.items())
+    n = N // sq(g)
+    ss.update((g * X, g * Y) for (X, Y) in cornacchia_primitive(D, n, fs.difference(vs.multiply(2))))
+  # return solutions in order
+  return sorted(ss)
 
 ######################################################################
 
@@ -158,25 +381,27 @@ def _diop_quad_c0(a, b):
   for t in irange(1, inf):
     yield (t * b, t * a)
 
-# a.X^2 + b.Y^2 = c [a > 0, b > 0, c > 0]
-def _diop_quad_bp_old(a, b, c):
-  # a brute force search
-  X = 0
-  while True:
-    r = c - a * X * X
-    if r < 0: break
-    (Y2, z) = divmod(r, b)
-    if z == 0:
-      Y = sqrtf(Y2)
-      if Y * Y == Y2: yield (X, Y)
-    X += 1
+# threshold at which we switch to Cornacchia's algorithm (or issue a warning)
+cornacchia_threshold = 50000
 
 # a.X^2 + b.Y^2 = c [a > 0, b > 0, c > 0]
-def _diop_quad_bp_new(a, b, c):
+def _diop_quad_bp(a, b, c):
+  if c < 0: return
+
   # start by considering possible Y values
   Y0 = sqrtf(divf(c, b))
   Y1 = sqrtc(divc(a * c, b * (a + b)))
-  #printf("Y = [{Y0}, {Y1}]")
+  # are there too many values to brute force?
+  if Y0 - Y1 >= cornacchia_threshold:
+    if a == 1 and b < c and gcd(b, c) == 1:
+      if verbose: printf("[pells] switching to Cornacchia (instead of brute forcing {n} values)", n=Y0 - Y1)
+      for XY in cornacchia(b, c): yield XY
+      return
+
+  # otherwise fall back to brute force
+  if verbose or Y0 - Y1 >= cornacchia_threshold: printf("[pells] brute forcing {n} values", n=Y0 - Y1)
+
+  # continue with brute force search
   X = None
   for Y in irange(Y0, Y1, step=-1):
     r = c - b * Y * Y
@@ -197,8 +422,6 @@ def _diop_quad_bp_new(a, b, c):
       Y = sqrtf(Y2)
       if Y * Y == Y2: yield (X, Y)
     X += 1
-
-_diop_quad_bp = _diop_quad_bp_new
 
 # X^2 - (dY)^2 = N [d > 0, N != 0]
 def _diop_quad_d2(d, N):
@@ -230,8 +453,11 @@ def _diop_quad_a1(D, N):
 def _diop_empty():
   if 0: yield None
 
+# threshold at w
+diop_quad_threshold = 10000
+
 # results (X, Y) for increasing X
-def diop_quad(a, b, c, maxC=10000, validate=0):
+def diop_quad(a, b, c, maxC=None, validate=0):
   if validate: (a, b, c) = map(as_int, (a, b, c))
   if a == 0 or b == 0: raise ValueError("diop_quad: invalid equation")
   if a < 0: (a, b, c) = (-a, -b, -c)
@@ -243,29 +469,33 @@ def diop_quad(a, b, c, maxC=10000, validate=0):
   if b > 0: return _diop_quad_bp(a, b, c)
   if a == 1: return _diop_quad_a1(-b, c)
 
-  # find a multiplier m, such that m.a is a square
+  # find a multiplier m, such that (m * a) is a square
   m = multiply(p for (p, e) in prime_factor(a) if e % 2 == 1)
   r = is_square(m * a)
-  def _fn():
+  if maxC is None: maxC = diop_quad_threshold
+  def fn():
     for (i, (X, Y)) in enumerate(_diop_quad_a1(m * -b, m * c), start=1):
       #printf("[diop_quad: considering: X={X} Y={Y}]")
       X = div(X, r)
       if X is not None: yield (X, Y)
-      if i == maxC: printf("diop_quad: WARNING: terminating search (after {maxC} candidates)"); break
-  return _fn()
+      if i == maxC:
+        printf("diop_quad: WARNING: terminating search (after {maxC} candidates)")
+        return
+  return fn()
 
 ######################################################################
 
 if enigma._namecheck(__name__):
   from enigma import (timer, arg, number as num)
 
-  (a, b, c, N, v) = (arg(-13, 0, num), arg(1, 1, num), arg(4, 2, num), arg(20, 3, num), arg(0, 4, num))
+  # c can be an expression
+  (a, b, c, N, v) = (arg(-13, 0, num), arg(1, 1, num), arg(4, 2, eval), arg(20, 3, num), arg(0, 4, num))
   printf("solving: {a}.X^2 + {b}.Y^2 = {c}")
   if v: verbose = 1
   if verbose: timer.start()
   for (i, (X, Y)) in enumerate(diop_quad(a, b, c, validate=1), start=1):
     r = a * X * X + b * Y * Y
-    printf("[{i}] X={X} Y={Y} -> {r}")
+    printf("[{i}] X={X} Y={Y} -> {r}{v}", v=(" [FAIL!]" if r != c else ""))
     if i == N: printf("[first {N} solutions]"); break
   else:
     printf("[all solutions]")
